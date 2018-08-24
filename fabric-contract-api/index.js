f0df3a07081a4f209f6bb82cdd7be01179d014a8
Matthew B White <whitemat@uk.ibm.com>
2018-08-24 13:55:06 +0100

    [FAB-11246] Transaction Functions in chaincode
    Change-Id: Ie94fe240a1d7fccc30003d785d50fb50bee9cc91
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

---
+++ fabric-contract-api/index.js
@@ -0,0 +1,19 @@
+/*
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+*/
+'use strict';
+
+/**
+ * @module fabric-contract-api
+ */
+
+/**
+ * This is the class that all contracts should extend. A Smart Contract will consist of one or more of these
+ * @see {@link fabric-contract-api.Contract}
+ */
+module.exports.Contract = require('./lib/contract.js');
+
+
+
