c1ee06859a5ab1fad3a44f1d28d64a7dc632decf
awjh-ibm <andrew.hurt1@ibm.com>
2018-10-04 15:55:11 +0100

    [FAB-12239] added CLI option for cc name
    Removed existing CLI handling and converted to use yargs
    
    Change-Id: Ida892031569390b3059b8e1310994a8707e6acb2
    Signed-off-by: awjh-ibm <andrew.hurt1@ibm.com>

--- fabric-shim/lib/contract-spi/bootstrap.js
+++ fabric-shim/lib/contract-spi/bootstrap.js
@@ -30,7 +30,6 @@ function register(contracts){
  * @ignore
  */
 function bootstrap(){
-
 	let jsonPath = path.resolve(__dirname,'..','..','..','..','package.json');
 	// let's find the package.json file
 	let json = require(jsonPath);
