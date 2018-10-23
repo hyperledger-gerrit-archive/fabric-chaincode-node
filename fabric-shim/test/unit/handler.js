510303b062bca28c465e403932e8b7cb22f49177
zhaochy <zhaochy_2015@hotmail.com>
2018-10-23 16:23:03 +0800

    [FAB-12538] chaincode handler for key-level metadata
    This CR implements the chaincode stub and handler to support setting
    and retrieval of key-level metadata.
    
    Change-Id: Ied5cb583e951cf23e391ea103d630c9f83296735
    Signed-off-by: zhaochy <zhaochy_2015@hotmail.com>

--- fabric-shim/test/unit/handler.js
+++ fabric-shim/test/unit/handler.js
@@ -967,7 +967,7 @@ describe('Handler', () => {
 				expect(_askPeerAndListenStub.firstCall.args[1]).to.deep.equal('DeleteState');
 			});
 
-			it ('should reject when _askPeerAndListen resolves', async () => {
+			it ('should reject when _askPeerAndListen rejects', async () => {
 				let handler = new Handler(mockChaincodeImpl, mockPeerAddress.unsecure);
 				let _askPeerAndListenStub = sandbox.stub(handler, '_askPeerAndListen').rejects();
 
@@ -977,6 +977,115 @@ describe('Handler', () => {
 				expect(_askPeerAndListenStub.firstCall.args.length).to.deep.equal(2);
 				expect(_askPeerAndListenStub.firstCall.args[0]).to.deep.equal(expectedMsg);
 				expect(_askPeerAndListenStub.firstCall.args[1]).to.deep.equal('DeleteState');
+			});
+		});
+
+		describe('handlePutStateMetadata', () => {
+			let key = 'theKey';
+			let collection = '';
+			let expectedMsg;
+			let metadataKey = 'VALIDATION_PARAMETER';
+			let ep = Buffer.from('theEP');
+
+			before(() => {
+				let serviceProto = Handler.__get__('_serviceProto');
+
+				const stateMetadata = new serviceProto.StateMetadata();
+				stateMetadata.setMetakey(metadataKey);
+				stateMetadata.setValue(ep);
+
+				let payload = new serviceProto.PutStateMetadata();
+				payload.setKey(key);
+				payload.setCollection(collection);
+				payload.setMetadata(stateMetadata);
+
+				expectedMsg = {
+					type: serviceProto.ChaincodeMessage.Type.PUT_STATE_METADATA,
+					payload: payload.toBuffer(),
+					channel_id: 'theChannelID',
+					txid: 'theTxID'
+				};
+			});
+
+			afterEach(() => {
+				Handler = rewire('../../../fabric-shim/lib/handler.js');
+				sandbox.restore();
+			});
+
+			it('should resolve when _askPeerAndListen resolves', async () => {
+				let handler = new Handler(mockChaincodeImpl, mockPeerAddress.unsecure);
+				let _askPeerAndListenStub = sandbox.stub(handler, '_askPeerAndListen').resolves('some response');
+
+				let result = await handler.handlePutStateMetadata(collection, key, metadataKey, ep, 'theChannelID', 'theTxID');
+
+				expect(result).to.deep.equal('some response');
+				expect(_askPeerAndListenStub.firstCall.args.length).to.deep.equal(2);
+				expect(_askPeerAndListenStub.firstCall.args[0]).to.deep.equal(expectedMsg);
+				expect(_askPeerAndListenStub.firstCall.args[1]).to.deep.equal('PutStateMetadata');
+			});
+
+			it('should reject when _askPeerAndListen rejects', async () => {
+				let handler = new Handler(mockChaincodeImpl, mockPeerAddress.unsecure);
+				let _askPeerAndListenStub = sandbox.stub(handler, '_askPeerAndListen').rejects();
+
+				let result = handler.handlePutStateMetadata(collection, key, metadataKey, ep, 'theChannelID', 'theTxID');
+
+				await expect(result).to.eventually.be.rejected;
+				expect(_askPeerAndListenStub.firstCall.args.length).to.deep.equal(2);
+				expect(_askPeerAndListenStub.firstCall.args[0]).to.deep.equal(expectedMsg);
+				expect(_askPeerAndListenStub.firstCall.args[1]).to.deep.equal('PutStateMetadata');
+			});
+		});
+
+		describe('handleGetStateMetadata', () => {
+			let key = 'theKey';
+			let collection = '';
+			let expectedMsg;
+			let metadataKey = 'VALIDATION_PARAMETER';
+			let ep = Buffer.from('theEP');
+
+			before(() => {
+				let serviceProto = Handler.__get__('_serviceProto');
+
+				let payload = new serviceProto.GetStateMetadata();
+				payload.setKey(key);
+				payload.setCollection(collection);
+
+				expectedMsg = {
+					type: serviceProto.ChaincodeMessage.Type.GET_STATE_METADATA,
+					payload: payload.toBuffer(),
+					channel_id: 'theChannelID',
+					txid: 'theTxID'
+				};
+			});
+
+			afterEach(() => {
+				Handler = rewire('../../../fabric-shim/lib/handler.js');
+				sandbox.restore();
+			});
+
+			it('should resolve when _askPeerAndListen resolves', async () => {
+				let handler = new Handler(mockChaincodeImpl, mockPeerAddress.unsecure);
+				let _askPeerAndListenStub = sandbox.stub(handler, '_askPeerAndListen').resolves('some response');
+
+				let result = await handler.handleGetStateMetadata(collection, key, 'theChannelID', 'theTxID');
+
+				expect(result).to.deep.equal('some response');
+				expect(_askPeerAndListenStub.firstCall.args.length).to.deep.equal(2);
+				expect(_askPeerAndListenStub.firstCall.args[0]).to.deep.equal(expectedMsg);
+				expect(_askPeerAndListenStub.firstCall.args[1]).to.deep.equal('GetStateMetadata');
+			});
+
+			it('should reject when _askPeerAndListen rejects', async () => {
+				let handler = new Handler(mockChaincodeImpl, mockPeerAddress.unsecure);
+				let _askPeerAndListenStub = sandbox.stub(handler, '_askPeerAndListen').rejects();
+
+				let result = handler.handleGetStateMetadata(collection, key, 'theChannelID', 'theTxID');
+
+				await expect(result).to.eventually.be.rejected;
+				expect(_askPeerAndListenStub.firstCall.args.length).to.deep.equal(2);
+				expect(_askPeerAndListenStub.firstCall.args[0]).to.deep.equal(expectedMsg);
+				expect(_askPeerAndListenStub.firstCall.args[1]).to.deep.equal('GetStateMetadata');
 			});
 		});
 
@@ -1670,6 +1779,36 @@ describe('Handler', () => {
 		});
 	});
 
+	describe('handleGetStateMetadata', () => {
+		let handleGetStateMetadata;
+		let payload;
+		let metaKey;
+		let ep;
+
+		before(() => {
+			handleGetStateMetadata = Handler.__get__('handleGetStateMetadata');
+			const serviceProto = Handler.__get__('_serviceProto');
+			ep = Buffer.from('someEP');
+			metaKey = 'theMetaKey';
+
+			const stateMetadata = new serviceProto.StateMetadata();
+			stateMetadata.setMetakey(metaKey);
+			stateMetadata.setValue(ep);
+
+			payload = new serviceProto.StateMetadataResult();
+			const entries = [stateMetadata];
+			payload.setEntries(entries);
+
+			payload = payload.toBuffer();
+		});
+
+		it('should success', () => {
+			const res = handleGetStateMetadata(payload);
+			expect(res).to.haveOwnProperty(metaKey);
+			expect(res[metaKey].toBuffer()).to.eql(ep);
+		});
+	});
+
 	describe('parseResponse', () => {
 		let qrDecodedPayload = 'qr decoded payload';
 		let ccDecodedPayload = 'cc decoded payload';
@@ -1835,6 +1974,16 @@ describe('Handler', () => {
 
 			expect(mockHistoryQueryIterator.calledWithNew).to.be.ok;
 			expect(mockHistoryQueryIterator.firstCall.args).to.deep.equal([handler, res.channel_id, res.txid, qrDecodedPayload]);
+		});
+
+		it('shold decode state metadata for GetStateMetadata', () => {
+			const mockHandleGetStateMetadata = sinon.stub().returns('decoded response');
+			Handler.__set__('handleGetStateMetadata', mockHandleGetStateMetadata);
+			let result = parseResponse(handler, res, 'GetStateMetadata');
+
+			expect(result).to.eql('decoded response');
+			sinon.assert.calledOnce(mockHandleGetStateMetadata);
+			sinon.assert.calledWith(mockHandleGetStateMetadata, res.payload);
 		});
 	});
 });
--- test/integration/test.js
+++ test/integration/test.js
@@ -6,6 +6,7 @@
 /* eslint-disable */
 'use strict';
 const shim = require('fabric-shim');
+const { KeyEndorsementPolicy } = require('fabric-shim/lib/utils/statebased');
 const ChaincodeCrypto = require('fabric-shim-crypto');
 const util = require('util');
 const chai = require('chai');
@@ -359,7 +360,6 @@ let Chaincode = class {
 	async test18(stub, args) {
 		console.log('test getQueryResultWithPagination()');
 
-
 		let query = {
 			selector: {
 				key: {
@@ -403,6 +403,28 @@ let Chaincode = class {
 	// return 'noreturn' to stop calling the shim error call..
 	async test20(stub, args) {
 		return 'noreturn'
+	}
+
+	async test21(stub, args) {
+		console.log('test 21, set state validation parameter');
+		const ep = new KeyEndorsementPolicy();
+		ep.addOrgs('MEMBER', 'Org1MSP');
+		await stub.setStateValidationParameter('key1', ep.getPolicy());
+	}
+
+	async test22(stub, args) {
+		console.log('test 22, get state validation parameter');
+
+		// should exists validation parameter ['Org1MSP'] for key1
+		const epBuffer = await stub.getStateValidationParameter('key1');
+		const ep = new KeyEndorsementPolicy(epBuffer);
+		console.log('state validation parameter --> ', ep.listOrgs());
+		ep.listOrgs().should.deep.eql(['Org1MSP']);
+
+		// should not exists validation parameter for key2
+		const epBuffer2 = await stub.getStateValidationParameter('key2');
+		console.log(epBuffer2);
+		assert.isUndefined(epBuffer2, 'key2 should not have validation parameter');
 	}
 };
 
