5d07d0a4cddbdd068cc9a0054b8585b53017298d
rameshthoomu <rameshbabu.thoomu@gmail.com>
2018-11-08 13:50:51 -0500

    FAB-12781 Update pipeline scripts
    Change-Id: I561f0c578fb4c1b5a5942a25fa3463ef5a192386
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- scripts/Jenkins_Scripts/Publish_NPM_Modules.sh
+++ scripts/Jenkins_Scripts/Publish_NPM_Modules.sh
@@ -7,23 +7,27 @@
 
 #################################################
 #Publish npm module as unstable after merge commit
-#npm publish --tag unstable
+#npm publish --tag $CURRENT_TAG
 #Run this "npm dist-tags ls $pkgs then look for
 #unstable versions
 #################################################
 
 npmPublish() {
-  if [ $RELEASE = "snapshot" ]; then
+
+if [[ "$CURRENT_TAG" = *"skip"* ]]; then
+      echo "----> Don't publish npm modules on skip tag"
+elif [[ "$CURRENT_TAG" = *"unstable"* ]]; then
+    # Get the version from npmjs of the  module
     echo
-    UNSTABLE_VER=$(npm dist-tags ls "$1" | awk '/unstable/{
+    UNSTABLE_VER=$(npm dist-tags ls "$1" | awk "/$CURRENT_TAG"":"/'{
     ver=$NF
     sub(/.*\./,"",rel)
     sub(/\.[[:digit:]]+$/,"",ver)
     print ver}')
 
     echo "===> UNSTABLE VERSION --> $UNSTABLE_VER"
-
-    UNSTABLE_INCREMENT=$(npm dist-tags ls "$1" | awk '/unstable/{
+## Increment unstable version here
+    UNSTABLE_INCREMENT=$(npm dist-tags ls "$1" | awk "/$CURRENT_TAG"":"/'{
     ver=$NF
     rel=$NF
     sub(/.*\./,"",rel)
@@ -32,30 +36,21 @@ npmPublish() {
 
     echo "===> Incremented UNSTABLE VERSION --> $UNSTABLE_INCREMENT"
 
-    if [ "$UNSTABLE_VER" = "$CURRENT_RELEASE" ]; then
-      # Replace existing version with Incremented $UNSTABLE_VERSION
-      sed -i 's/\(.*\"version\"\: \"\)\(.*\)/\1'$UNSTABLE_INCREMENT\"\,'/' package.json
-      npm publish --tag unstable
-    else
-      # Replace existing version with $CURRENT_RELEASE
-      sed -i 's/\(.*\"version\"\: \"\)\(.*\)/\1'$CURRENT_RELEASE\"\,'/' package.json
-      npm publish --tag unstable
-    fi
+else
+    # Publish node modules on latest tag
+    echo -e "\033[32m ========> PUBLISH --> $RELEASE_VERSION" "\033[0m"
+    npm publish --tag $CURRENT_TAG
 
-  else
-
-      if [[ "$RELEASE" =~ alpha*|preview*|beta*|rc*|^[0-9].[0-9].[0-9]$ ]]; then
-         echo "----> Publish $RELEASE from fabric-chaincode-node-npm-release-x86_64"
-      fi
 fi
 }
 versions() {
 
   CURRENT_RELEASE=$(cat package.json | grep version | awk -F\" '{ print $4 }')
-  echo "===> Current Version --> $CURRENT_RELEASE"
+  echo -e "\033[32m ========> PUBLISH --> $RELEASE_VERSION" "\033[0m"
 
   RELEASE=$(cat package.json | grep version | awk -F\" '{ print $4 }' | cut -d "-" -f 2)
-  echo "===> Current Release --> $RELEASE"
+  echo -e "\033[32m ===> Current Version --> $RELEASE_VERSION" "\033[0m"
+
 }
 
 cd $WORKSPACE/gopath/src/github.com/hyperledger/fabric-chaincode-node
@@ -63,12 +58,15 @@ npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN
 
 cd fabric-shim
 versions
+# Publish fabric-shim npm module
 npmPublish fabric-shim
 
 cd ../fabric-shim-crypto
 versions
+# Publish fabric-shim-crypto npm module
 npmPublish fabric-shim-crypto
 
 cd ../fabric-contract-api
 versions
+# Publish fabric-contract-api npm module
 npmPublish fabric-contract-api
