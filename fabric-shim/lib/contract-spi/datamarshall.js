69189d5eb8d7a739a7210696ee1cde7dd7636cf7
Matthew B White <whitemat@uk.ibm.com>
2018-11-23 17:28:21 +0000

    refactor
    Change-Id: Ic68abd0075a2b64c3e2711d7c52ff65c67df51dc
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- fabric-shim/lib/contract-spi/datamarshall.js
+++ fabric-shim/lib/contract-spi/datamarshall.js
@@ -5,6 +5,8 @@
 */
 'use strict';
 
+const Logger = require('../logger');
+const logger = Logger.getLogger('contracts-spi/datamarshall.js');
 
 module.exports = class DataMarshall {
 
@@ -14,13 +16,16 @@ module.exports = class DataMarshall {
      * @param {String} requestedSerializer name of the requested serializer
      * @param {Object} serializers mapping of names to the implementation of the serializers
      */
-    constructor(requestedSerializer, serializers) {
+    constructor(requestedSerializer, serializers, chaincode) {
         let cnstr = serializers[requestedSerializer];
         if (typeof cnstr === 'string') {
             cnstr = require(cnstr);
         }
 
         this.serializer = new (cnstr)();
+
+
+        // this.schemas = chaincode.contractImplementations.schemas;
     }
 
     /** Convert the result into a buffer than can be hand off to grpc (via the shim)
@@ -44,4 +49,55 @@ module.exports = class DataMarshall {
         return this.serializer.fromBuffer(result);
     }
 
+    fromWire(result, type) {
+
+    }
+
+    /**
+     * Process all the parameters
+     *
+     * @param {object} fn Function currently being called
+     * @param {array} parameters Parameters as passed from the shim
+     * @return {array} of parameters that can be passed to the actual tx function
+     */
+    handleParameters(fn, parameters) {
+        const expectedParams = fn.parameters;
+        if (!expectedParams) {
+            return [];
+        }
+
+        if (expectedParams.length !== fn.parameters.length) {
+            throw new Error(`Expected ${expectedParams.length} parameters, but ${fn.parameters.length} have been supplied`);
+        }
+
+        const returnParams = [];
+
+        // check each parameter matches the type and then demarshall
+        for (let i = 0; i < fn.parameters.length; i++) {
+            const supplied = parameters[i];
+            const expected = expectedParams[i];
+            logger.debug(expected);
+            logger.debug(supplied);
+            // check the type
+            const schema = expected.schema;
+            const name = expected.name;
+            if (schema.type) {
+                // simple type conversion here
+                // TODO: better support here; refactor of internal data structure needed
+                switch (schema.type) {
+                    case 'string':
+                        returnParams.push(supplied);
+                        break;
+                    default:
+                        returnParams.push(JSON.parse(supplied));
+                        break;
+                }
+                returnParams.push(supplied);
+            } else {
+                throw new Error('incorrect type information');
+            }
+        }
+
+        return returnParams;
+    }
 };
--- fabric-shim/lib/contract-spi/systemcontract.js
+++ fabric-shim/lib/contract-spi/systemcontract.js
@@ -27,41 +27,16 @@ class SystemContract extends Contract {
 	 *
 	 * @param {Object} chaincode
 	 */
-    _setChaincode(chaincode) {
-        this.chaincode = chaincode;
+    setMetadata(metadata) {
+        this.metadata = metadata;
     }
     /**
      * Gets meta data associated with this Chaincode deployment
      */
     async GetMetadata() {
-        let metadata;
-        const opts = StartCommand.getArgs(yargs);
-        const modPath = path.resolve(process.cwd(), opts['module-path']);
-        const metadataPath = path.resolve(modPath, 'contract-metadata', 'metadata.json');
-        const pathCheck = await fs.pathExists(metadataPath);
-
-        if (pathCheck) {
-            metadata = await this._loadAndValidateMetadata(metadataPath);
-        } else {
-            metadata = this.chaincode.getContracts();
-        }
-        return metadata;
+        return this.metadata;
     }
 
-
-    async _loadAndValidateMetadata(metadataPath) {
-        const rootPath = path.dirname(__dirname);
-        const metadata = (await fs.readFile(metadataPath)).toString();
-        const schema = (await fs.readFile(path.join(rootPath, '../../fabric-contract-api/schema/contract-schema.json'))).toString();
-        const ajv = new Ajv({schemaId: 'id'});
-        ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
-        const valid = ajv.validate(JSON.parse(schema), JSON.parse(metadata));
-        if (!valid) {
-            throw new Error('Contract metadata does not match the schema: ' + JSON.stringify(ajv.errors));
-        }
-
-        return metadata;
-    }
 }
 
 module.exports = SystemContract;
