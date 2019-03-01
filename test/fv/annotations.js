9e80eefea804bd1b710c7d5065cc86f8d3c7af91
SamRasha <samanrasha@hotmail.co.uk>
2019-03-01 16:23:54 +0000

    [FAB-14144] Integration test reform
    Change-Id: I3066d38fdf457f7787f1bbf7d92bdc772a144ca2
    Signed-off-by: SamRasha <samanrasha@hotmail.co.uk>

--- test/fv/annotations.js
+++ test/fv/annotations.js
@@ -1,3 +1,10 @@
+/*
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+*/
+'use-strict';
+
 const fs = require('fs');
 const path = require('path');
 const Ajv = require('ajv');
@@ -5,19 +12,22 @@ const chai = require('chai');
 chai.use(require('chai-as-promised'));
 const expect = chai.expect;
 const utils = require('./utils');
-const {SHORT_STEP, LONG_STEP} = utils.TIMEOUTS;
+const {SHORT_STEP, MED_STEP, LONG_STEP} = utils.TIMEOUTS;
 
 describe('Typescript chaincode', () => {
     const suite = 'annotations';
-    before(async function() {
+
+    before(async function () {
         this.timeout(LONG_STEP);
+
         return utils.installAndInstantiate(suite);
     });
 
 
     describe('Scenario', () => {
+
         it('should write an asset', async function () {
-            this.timeout(LONG_STEP);
+            this.timeout(MED_STEP);
             await utils.invoke(suite, 'TestContract:createAsset', ['GLD', 'GOLD_BAR', '100']);
             const payload = JSON.parse(await utils.query(suite, 'TestContract:getAsset', ['GLD']));
             expect(payload).to.eql({id: 'GLD', name: 'GOLD_BAR', value: 100});
@@ -39,5 +49,7 @@ describe('Typescript chaincode', () => {
             const expectedMetadata = fs.readFileSync(path.join(__dirname, './annotations/src/test_contract/expected-metadata.json'));
             expect(payload).to.eql(JSON.parse(expectedMetadata));
         });
+
     });
+
 });
