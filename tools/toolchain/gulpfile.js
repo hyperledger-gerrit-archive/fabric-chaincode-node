35c916c512b4aab149311ae9b2ec3a875c7fb73b
Matthew B White <whitemat@uk.ibm.com>
2019-09-12 14:32:20 +0100

    Refactor to RUSH
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>
    Change-Id: Ie873615a4285909c082abdc3c80a77439dda6850

---
+++ tools/toolchain/gulpfile.js
@@ -0,0 +1,16 @@
+/*
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+*/
+// const {testScenario} = require('./build/test/scenario');
+
+// exports.scenario = testScenario;
+// exports.docs = docs;
+// exports.default = series(lint, protos, test_schema, unittest);
+
+// backward compatable names for the Jenkins build
+// task('channel-init', startFabric);
+// task('test-headless', series(lint, protos, test_schema, unittest));
+// task('test-e2e', series(testfvshim, testScenario));
+// task('test-devmode', series(test-scenario-devmode));
